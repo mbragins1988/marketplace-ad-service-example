af7f91962ac883abb8627f16f51df834045a5865
Mikhail <mbragins@mail.ru>
2026-06-03 09:45:24 +0500

    fix run outbox

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ cd /app
 echo "Applying database migrations..."
 uv run alembic upgrade head
 
-# Запускаем воркер в фоновом режиме
-uv run python -m bin.outbox_relay &
+# Запускаем воркер в фоне
+nohup uv run python -m bin.outbox_relay &
 
 # Запускаем API
 exec uv run python -m bin.api
